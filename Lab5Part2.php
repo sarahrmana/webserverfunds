e9c869629ffe268ba67618b320b21475030d7092
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-09-24 11:59:34 -0400

    Update Lab5Part2.php

--- Lab5Part2.php
+++ Lab5Part2.php
@@ -10,12 +10,12 @@
         <p>Name of user: <?=htmlspecialchars($_POST['fname']) . " " . htmlspecialchars($_POST['lname'])?></p>
         <p>Age of user: <?=(int)$_POST['age']?> </p>
         <p>The amount of hours they play games a week: <?=htmlspecialchars($_POST['hours'])?> </p> >
-        <?php if(isset($_POST['rpg'])
+        <?php if(isset($_POST['rpg']))
             echo "They play RPG games";
         else   
             echo "They do not play RPG games";
         ?>
-        <?php if(isset($_POST['openworld'])
+        <?php if(isset($_POST['openworld']))
             echo "They play open world games";
         else   
             echo "They do not play open world games";
