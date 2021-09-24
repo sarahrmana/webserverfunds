7a63a2edd36db97edd059b667ba0df9cdfeb1369
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-09-24 10:41:50 -0400

    Update Lab5Part2.php

--- Lab5Part2.php
+++ Lab5Part2.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> PHP Page! </title>
+        <title> What is my user agent </title>
     </head>
     <body>
+        <p> Your browser is <?=$_SERVER['HTTP_USER_AGENT']?> </php>
         <p><?= var_dump($_SERVER) ?></p>
     </body>
 <html>
