1d71fcc523a2260ce1ecb099ad4b506e627c45c0
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-09-24 12:22:00 -0400

    Update Lab5Part2.php

--- Lab5Part2.php
+++ Lab5Part2.php
@@ -12,30 +12,44 @@
         <p>The amount of hours they play games a week: <?=htmlspecialchars($_POST['hours'])?> </p>
         <?php if(isset($_POST['rpg']))
             echo "They play RPG games";
+        else   
+            echo "They do not play RPG games"; 
         ?>
         <br>
         <?php if(isset($_POST['openworld']))
             echo "They play open world games";
+        else   
+            echo "They do not play open world games";
         ?>
         <br>
         <?php if(isset($_POST['fps']))
             echo "They play FPS games";
+        else   
+            echo "They do not play FPS games";
         ?>
         <br>
         <?php if(isset($_POST['sports']))
             echo "They play sport games";
+        else   
+            echo "They do not play sport games";
         ?>
         <br>
         <?php if(isset($_POST['mmo']))
             echo "They play mmo games";
+        else   
+            echo "They do not play mmo games";
         ?>
         <br>
         <?php if(isset($_POST['puzzle']))
             echo "They play puzzle games";
+        else   
+            echo "They do not play puzzle games";
         ?>
         <br>
         <?php if(isset($_POST['racing']))
             echo "They play racing games";
+        else   
+            echo "They do not play racing games";
         ?>
         <br>
 
