aabf90304df0a3d22de09cf5007f092e4eb886ff
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-09-24 11:25:10 -0400

    Update Lab5Part2.php

--- Lab5Part2.php
+++ Lab5Part2.php
@@ -7,8 +7,8 @@
         <p><?= var_dump($_GET) ?></p>
         <p><?= var_dump($_POST) ?></p>
 
-        <!-- <p>Name of user: <?=htmlspecialchars($POST['fname']) . " " . htmlspecialchars($_POST['lname'])?></p>
-        <p>Age of user: <?=htmlspecialchars($POST)['age'])?> </p>
-        <p>The amount of hours they play games a week: <?=htmlspecialchars($POST)['hours'])?> </p> -->
+        <!-- <p>Name of user: <?=htmlspecialchars($_POST['fname']) . " " . htmlspecialchars($_POST['lname'])?></p>
+        <p>Age of user: <?=htmlspecialchars($_POST['age'])?> </p>
+        <p>The amount of hours they play games a week: <?=htmlspecialchars($_POST['hours'])?> </p> -->
     </body>
 <html>
