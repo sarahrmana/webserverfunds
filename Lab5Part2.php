5f5bc2b7235f72ac00fd72422f77ff1d4efeb3ec
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-09-24 10:36:01 -0400

    Update Lab5Part2.php

--- Lab5Part2.php
+++ Lab5Part2.php
@@ -1,1 +1,9 @@
-<p><?= var_dump($_SERVER) ?></p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> PHP Page! </title>
+    </head>
+    <body>
+        <p><?= var_dump($_SERVER) ?></p>
+    </body>
+<html>
