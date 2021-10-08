25509bbea23ba7e7bdf12657d0ff8a5766146639
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-10-08 12:22:45 -0400

    Create phpmariadb.php

---
+++ phpmariadb.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+ <body>
+    <?php
+    $server = "localhost";
+    $username = "pi";
+    $password = "voidnul1";
+    $database = "videogames";
+    $conn = mysqli_connect($server, $username, $password, $database);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";?>
+
+    $sql = "select * from fave_games;";
+    $result = mysqli_query($conn, $sql);
+
+    foreach ($result as $row) {
+        echo "id: " . $row["id"]. " | Name: " . $row["game"]. " " .$row["genre"] . "\n";}
+</body>
+
