1bc52bff6a24b394532efe0fea4572b4ac04295e
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-10-12 11:30:37 -0400

    Create mariadbresults.php

---
+++ mariadbresults.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Form Submissions </title>
+    </head>
+    <body>
+        <h1>Form Submissions</h1>
+        <p>Selected game: <?=htmlspecialchars($_POST['game'])?></p>
+    </body>
+</html>
