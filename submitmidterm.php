1dd8c723c11c70e4123da0f880c09e31f1bd147d
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-10-12 10:42:20 -0400

    Update submitmidterm.php

--- submitmidterm.php
+++ submitmidterm.php
@@ -4,8 +4,6 @@
         <title> Form Submissions </title>
     </head>
     <body>
-        <p><?= var_dump($_POST) ?></p>
-
         <p>Selected item: <?=htmlspecialchars($_POST['item'])?></p>
         <p>Desired quantity: <?=(int)$_POST['quantity']?> </p>
     </body>
