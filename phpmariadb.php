d8160c38a15d5bba7083dfcedce17ea3b4fd75ba
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-10-12 11:39:24 -0400

    Update phpmariadb.php

--- phpmariadb.php
+++ phpmariadb.php
@@ -13,11 +13,11 @@
     }
     echo "Connected successfully";
     echo "<br/>";
-    $sql = "select * from fave_games;";
+  //  $sql = "select * from fave_games;";
     $result = mysqli_query($conn, $sql);
 
-   // foreach ($result as $row) {
-    //    echo "id: {$row['id']} | Name: {$row['game']} | Genre: {$row["genre"]} | Price: \${$row["price"]} </br>";
+    foreach ($result as $row) {
+        echo "id: {$row['id']} | Name: {$row['game']} | Genre: {$row["genre"]} | Price: \${$row["price"]} </br>";
     }
     ?>
     <form action="mariadbresults.php" method=POST>
