75b8c6d8bab2168db9492b96eb6641f48fa6eb06
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-10-12 12:36:07 -0400

    Update mariadbresults.php

--- mariadbresults.php
+++ mariadbresults.php
@@ -33,7 +33,8 @@
         You added <?=htmlspecialchars($_POST['newgame'])?> with a genre of <?=htmlspecialchars($_POST['newgenre'])?> and a price of <?=(int)$_POST['price']?>
         
           <?php
-            $sql = "INSERT INTO fave_games (game,genre,price) VALUES ($newgame, $newgenre, $newprice);";
+            $sql = "INSERT INTO fave_games (game,genre,price) 
+                    VALUES ($newgame, $newgenre, $newprice)";
             $result = mysqli_query($conn,$sql);
             echo $result ? "Success!" : "Failure: " . mysqli_error($conn);
             mysqli_close($conn); 
