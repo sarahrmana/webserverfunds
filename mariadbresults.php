f968e9b36cd35e14e2bb0ee6c60470d4745025ba
Sarah Manascu <sarah.manascu@dawsoncollege.qc.ca>
2021-10-12 11:37:56 -0400

    Update mariadbresults.php

--- mariadbresults.php
+++ mariadbresults.php
@@ -6,5 +6,6 @@
     <body>
         <h1>Form Submissions</h1>
         <p>Selected game: <?=htmlspecialchars($_POST['game'])?></p>
+        <p>Genre of game: <?htmlspecialchars($_POST)['genre'])?></p>
     </body>
 </html>
